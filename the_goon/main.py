5484ca734c604f99983dd39faa79f37bf9329316
yoonthegoon <mby6mk+dqn1bcda2lwdykk9by4m0hhb7w@grr.la>
2021-04-03 14:19:52 -0400

    Change commands to cogs

--- the_goon/main.py
+++ the_goon/main.py
@@ -23,9 +23,9 @@ async def on_ready():
         print(f'\t{guild}')
 
 
-for file in os.listdir('./commands'):
+for file in os.listdir('cogs'):
     if file.endswith('.py'):
-        bot.load_extension(f'commands.{file[:-3]}')
+        bot.load_extension(f'cogs.{file[:-3]}')
 
 
 bot.run(os.getenv('DISCORD_TOKEN'))
