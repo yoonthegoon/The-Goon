8efeb78872dd5844928666773fb398c185fdae66
yoonthegoon <mby6mk+dqn1bcda2lwdykk9by4m0hhb7w@grr.la>
2021-04-07 11:20:32 -0400

    add prefix command

---
+++ cogs/configuration.py
@@ -0,0 +1,31 @@
+import discord
+from discord.ext import commands
+
+
+embed = discord.Embed(color=0x037f03)
+embed.set_author(name="The Goon", url="https://github.com/yoonthegoon/The-Goon",
+                 icon_url="https://cdn.discordapp.com/avatars/783779669979693117/84be9f2ab1b9bbb56a6c6c113cae7340.png")
+
+
+class Configuration(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def prefix(self, ctx, pre: str):
+        """Change command prefix."""
+
+        embed.title = f'prefix: {pre}'
+
+        try:
+            self.bot.command_prefix = pre
+            embed.description = 'Success?'
+
+        except Exception as e:
+            embed.description = f'ERROR: {e}'
+
+        await ctx.reply(embed=embed, mention_author=False)
+
+
+def setup(bot):
+    bot.add_cog(Configuration(bot))
