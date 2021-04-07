4e4cab0368e3cdfdfb712ba8b4e888ec55689881
yoonthegoon <mby6mk+dqn1bcda2lwdykk9by4m0hhb7w@grr.la>
2021-04-06 20:13:44 -0400

    Add chance cog

---
+++ cogs/chance.py
@@ -0,0 +1,68 @@
+import discord
+from discord.ext import commands
+from random import *
+
+
+embed = discord.Embed(color=0x037f03)
+embed.set_author(name="The Goon", url="https://github.com/yoonthegoon/The-Goon",
+                 icon_url="https://cdn.discordapp.com/avatars/783779669979693117/84be9f2ab1b9bbb56a6c6c113cae7340.png")
+
+
+class Chance(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def flip(self, ctx, limit: int):
+        """Flips a coin a given number of times."""
+        embed.title = f'flip: {" ".join(limit)}'
+
+        try:
+            flip_results = [choice(('Heads', 'Tails')) for _ in limit]
+            if len(flip_results) == 1:
+                embed.description = flip_results[0]
+            else:
+                embed.description = ', '.join(flip_results)
+                flip_results.sort()
+                from itertools import groupby
+                result = [len(list(group)) for key, group in groupby(flip_results)]
+                embed.description += f'\n(Heads: {result[0]}, Tails: {result[1]})'
+
+        except Exception as e:
+            embed.description = f'ERROR: {e}'
+
+        await ctx.reply(embed=embed, mention_author=False)
+
+    @commands.command()
+    async def choose(self, ctx, *expressions: str):
+        """Choose one out of given options."""
+        embed.title = f'choose: {" ".join(expressions)}'
+
+        try:
+            embed.description = choice(expressions)
+
+        except Exception as e:
+            embed.description = f'ERROR: {e}'
+
+        await ctx.reply(embed=embed, mention_author=False)
+
+    @commands.command()
+    async def roll(self, ctx, dice):
+        """Rolls dice in NdN format"""
+        embed.title = f'roll: {" ".join(dice)}'
+
+        try:
+            rolls, limit = map(int, dice.split('d'))
+            roll_results = [randint(1, limit) for _ in range(rolls)]
+            embed.description = sum(roll_results)
+            if len(roll_results) > 1:
+                embed.description += f' = ({" + ".join(map(str, roll_results))})'
+
+        except Exception as e:
+            embed.description = f'ERROR: {e}'
+
+        await ctx.reply(embed=embed, mention_author=False)
+
+
+def setup(bot):
+    bot.add_cog(Chance(bot))
