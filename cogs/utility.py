17ebce02b9efa1ea38a9b2b7eb6acbe1620d1ffa
yoonthegoon <mby6mk+dqn1bcda2lwdykk9by4m0hhb7w@grr.la>
2021-04-20 17:23:00 -0400

    make define and weather commands objectively better

--- cogs/utility.py
+++ cogs/utility.py
@@ -31,13 +31,12 @@ class Utility(commands.Cog):
 
             r = define(word)
 
-            embed.title = 'wordnik'
             embed.url = f'https://www.wordnik.com/words/{word}'
-            embed.add_field(name="source", value=r['source'], inline=False)
             for li in r['ul']:
                 if li > 4:
                     break
                 embed.add_field(name=r['ul'][li][0]['abbr'], value=r['ul'][li][1], inline=False)
+            embed.set_footer(text=r['source'])
 
         except Exception as e:
             embed.colour = 0x7f0003
@@ -48,26 +47,28 @@ class Utility(commands.Cog):
     @commands.command()
     async def weather(self, ctx, *city):
         """Gives the current weather for a city"""
+        q = " ".join(city)
 
-        embed = discord.Embed(title=f'weather: {", ".join(city)}', color=0x037f03)
+        embed = discord.Embed(title=f'weather: {q}', color=0x037f03)
         embed.set_author(name="The Goon", url="https://github.com/yoonthegoon/The-Goon",
                          icon_url="https://cdn.discordapp.com/avatars/783779669979693117/84be9f2ab1b9bbb56a6c6c113cae7340.png")
-        
+
         try:
             api_key = os.getenv('OWM_TOKEN')
 
             r = requests.get(f'https://api.openweathermap.org/data/2.5/weather'
-                             f'?q={",".join(city)}&appid={api_key}&units=imperial')
+                             f'?q={",".join(q.split(", "))}&appid={api_key}&units=imperial')
             json = r.json()
             print(json)
 
+            embed.url = f'https://www.openstreetmap.org/#map=12/{json["coord"]["lat"]}/{json["coord"]["lon"]}'
             embed.description = f'{json["main"]["temp"]:.1f}°F. Feels like {json["main"]["feels_like"]:.0f}°F. {json["weather"][0]["description"].capitalize()}'
             embed.set_thumbnail(url=f'https://openweathermap.org/img/wn/{json["weather"][0]["icon"]}@2x.png')
             embed.add_field(name='High / Low', value=f'{json["main"]["temp_max"]:.1f}°F/{json["main"]["temp_min"]:.1f}°F', inline=True)
             embed.add_field(name='Wind', value=f'{json["wind"]["speed"]}mph {degrees_to_cardinal(json["wind"]["deg"])}', inline=True)
             embed.add_field(name='Humidity', value=f'{json["main"]["humidity"]}%', inline=True)
             embed.add_field(name='Pressure', value=f'{json["main"]["pressure"] * 0.02953:.1f}in', inline=True)
-            embed.add_field(name='Visibility', value=f'{json["visibility"] * 0.0006213712:.1f}mi', inline=True)
+            embed.add_field(name='Visibility', value=f'{json["visibility"] * 0.0006213712:.1f}mi' if json["visibility"] != 10000 else 'High', inline=True)
 
         
         except Exception as e:
